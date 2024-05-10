011a3067089ce90c93a01f599f5c34fb2d4b9dbc
sano_yuya <k021g1256@g.neec.ac.jp>
2024-05-10 16:24:45 +0900

    コメントアウト追加

--- Source/Player/PlayerAction.h
+++ Source/Player/PlayerAction.h
@@ -11,10 +11,12 @@ private:
 	IFE::Vector3 pos_ = { 0,0,0 };
 	IFE::Vector3 frontVec_ = {};
 
+#pragma region カメラ
 	IFE::Vector3 cameraPos_ = {};
 	IFE::Vector2 cameraAngle_ = {};
 
 	IFE::Camera* actionCamera_ = nullptr;
+#pragma endregion カメラ
 
 	bool attackFlag_ = false;
 	const uint8_t attackTime_ = 10;
@@ -22,27 +24,59 @@ private:
 
 public:
 
+	/// <summary>
+	/// 初期化処理
+	/// </summary>
 	void Initialize();
 
+	/// <summary>
+	/// 更新処理
+	/// </summary>
 	void Update();
 
+	/// <summary>
+	/// 描画処理
+	/// </summary>
 	void Draw();
 
+	/// <summary>
+	/// 終了処理
+	/// </summary>
 	void Finalize();
 
 	void OnColliderHit(IFE::ColliderCore collider);
 
+
+	/// <summary>
+	/// Playerクラスで呼ぶ操作全般更新処理
+	/// </summary>
 	void MoveUpdate();
 
+	/// <summary>
+	/// positionの取得
+	/// </summary>
+	/// <returns></returns>
 	const IFE::Vector3 GetPos();
 
 private:
 
+	/// <summary>
+	/// 移動処理
+	/// </summary>
 	void Move();
 
+	/// <summary>
+	/// 回転処理
+	/// </summary>
 	void Rotation();
 
+	/// <summary>
+	/// カメラの更新処理
+	/// </summary>
 	void CameraUpdate();
 
+	/// <summary>
+	/// 攻撃処理
+	/// </summary>
 	void Attack();
 };
