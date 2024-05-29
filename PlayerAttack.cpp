3f7314e4ea7de8b2096517bb80702dbc46f92bef
sano_yuya <k021g1256@g.neec.ac.jp>
2024-05-29 14:48:03 +0900

    自機の攻撃追加

--- PlayerAttack.cpp
+++ PlayerAttack.cpp
@@ -1,1 +1,17 @@
 #include "PlayerAttack.h"
+#include "ObjectManager.h"
+#include "Transform.h"
+
+void PlayerAttack::Initialize()
+{
+	objectPtr_->DrawFlag_ = false;
+	objectPtr_->SetColor({ 0,0,1,1 });
+}
+
+void PlayerAttack::Update()
+{
+}
+
+void PlayerAttack::Finalize()
+{
+}
