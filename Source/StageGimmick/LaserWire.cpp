4ea6881262f068eae113b8b9f677f20e643a7a76
k021g1165@g.neec.ac.jp <k021g1165@g.neec.ac.jp>
2024-06-03 17:38:20 +0900

    ギミックの中身作った

--- Source/StageGimmick/LaserWire.cpp
+++ Source/StageGimmick/LaserWire.cpp
@@ -1,14 +1,24 @@
 #include "LaserWire.h"
-#include "Collider.h"
+
+#include "Transform.h"
 
 void LaserWire::Initialize()
 {
-
+	cols_ = objectPtr_->GetComponent<IFE::Collider>();
 }
 
 void LaserWire::Update()
 {
-
+	for (int16_t i = 0; i < poss_.size(); i++)
+	{
+		auto col = cols_->GetCollider(i);
+		if (col == nullptr)
+		{
+			col = cols_->AddCollider();
+		}
+		col->SetOffsetPosition(poss_[i]);
+		col->SetOffsetScale(scales_[i]);
+	}
 }
 
 void LaserWire::Draw()
@@ -26,3 +36,52 @@ void LaserWire::OnColliderHit(IFE::ColliderCore collider)
 	isHit_ = true;
 }
 
+#ifdef EditorMode
+#include "ImguiManager.h"
+void LaserWire::ComponentDebugGUI()
+{
+	
+	IFE::ImguiManager* gui = IFE::ImguiManager::Instance();
+	gui->DragFloat3GUI(&transform_->position_, "points", 0.25f, -1000, 1000);
+	gui->DragVectorFloat3GUI(poss_, "pos", {0,0,0}, 0.25f, 0, 1000);
+	gui->DragVectorFloat3GUI(rots_, "rot", {0,0,0}, 0.25f, 0, 1000);
+	gui->DragVectorFloat3GUI(scales_, "scale", {0,0,0}, 0.25f, 0, 1000);
+
+	if (oldposSize != poss_.size())
+	{
+		rots_.resize(poss_.size());
+		scales_.resize(poss_.size());
+		oldposSize = (int32_t)poss_.size();
+		oldrotSize = (int32_t)rots_.size();
+		oldscaleSize = (int32_t)scales_.size();
+	}
+	if (oldrotSize != rots_.size())
+	{
+		poss_.resize(rots_.size());
+		scales_.resize(rots_.size());
+		oldposSize = (int32_t)poss_.size();
+		oldrotSize = (int32_t)rots_.size();
+		oldscaleSize = (int32_t)scales_.size();
+	}
+	if (oldscaleSize != scales_.size())
+	{
+		poss_.resize(scales_.size());
+		rots_.resize(scales_.size());
+		oldposSize = (int32_t)poss_.size();
+		oldrotSize = (int32_t)rots_.size();
+		oldscaleSize = (int32_t)scales_.size();
+	}
+
+	
+}
+
+void LaserWire::OutputComponent(nlohmann::json& json)
+{
+	json;
+}
+#endif
+
+void LaserWire::LoadingComponent(nlohmann::json& json)
+{
+	json;
+}
--- Source/StageGimmick/LaserWire.h
+++ Source/StageGimmick/LaserWire.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "Component.h"
+#include "Object3D.h"
+#include "Collider.h"
 
 class LaserWire:public IFE::Component
 {
@@ -20,9 +22,29 @@ public:
 
 	void OnColliderHit(IFE::ColliderCore collider);
 
+#ifdef EditorMode
+
+	void ComponentDebugGUI()override;
+	void OutputComponent(nlohmann::json& json)override;
+#endif
+	void LoadingComponent(nlohmann::json& json)override;
+
+
 private:
 
-	
+	IFE::Float3 pos_;
+
+	std::vector<IFE::Float3> poss_;
+	std::vector<IFE::Float3> rots_;
+	std::vector<IFE::Float3> scales_;
+
+	std::vector<IFE::Object3D> objects_;
+
+	int32_t oldposSize = 0;
+	int32_t oldrotSize = 0;
+	int32_t oldscaleSize = 0;
+
+	IFE::Collider* cols_;
 
 };
 
