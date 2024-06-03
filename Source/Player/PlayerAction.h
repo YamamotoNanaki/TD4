371639f8e5f81a089b42a3b8e7d836af6a9bdd3a
sano_yuya <k021g1256@g.neec.ac.jp>
2024-06-03 14:21:12 +0900

    方向ベクトルを取得できるようにした

--- Source/Player/PlayerAction.h
+++ Source/Player/PlayerAction.h
@@ -12,7 +12,6 @@ private:
 	IFE::Vector3 pos_ = { 0,0,0 };
 
 	//正面ベクトルの作成
-	IFE::Vector3 cameraFrontVec_ = {};
 	IFE::Vector3 frontVec_ = {};
 	//仮ベクトル
 	IFE::Vector3 temporaryVec_ = { 0,1,0 };
@@ -84,6 +83,8 @@ public:
 	/// </summary>
 	/// <returns></returns>
 	const bool GetAttackFlag();
+
+	const IFE::Vector3 GetFrontVec();
 
 private:
 
