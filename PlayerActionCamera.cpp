b4b16491225173f1b28db5a44ece5b99a08f819d
sano_yuya <k021g1256@g.neec.ac.jp>
2024-06-10 15:59:05 +0900

    当たり判定の距離がとれれば多分完成

--- PlayerActionCamera.cpp
+++ PlayerActionCamera.cpp
@@ -1,11 +1,23 @@
 #include "PlayerActionCamera.h"
+#include"Transform.h"
+#include "Collider.h"
+#include "Input.h"
+#include "IFETime.h"
+#include"Object3D.h"
 
 void PlayerActionCamera::Initialize()
 {
+	actionCamera_ = IFE::CameraManager::Instance()->GetCamera("ActionCamera");
+	cameraAngle_.y = 180.0f;
+
+	objectPtr_->DrawFlag_ = false;
 }
 
 void PlayerActionCamera::Update()
 {
+	float distance = 15.0f;
+	float adjustedTimeValue = 15.0f;
+	IFE::Complement(distance_, distance, adjustedTimeValue);
 }
 
 void PlayerActionCamera::Draw()
@@ -16,6 +28,101 @@ void PlayerActionCamera::Finalize()
 {
 }
 
-void PlayerActionCamera::OnColliderHit(IFE::ColliderCore collider)
+void PlayerActionCamera::OnColliderHit(IFE::ColliderCore* myCollider, IFE::ColliderCore* hitCollider)
 {
+	myCollider;
+	if (hitCollider->attribute_ == static_cast<uint16_t>(IFE::Attribute::LANDSHAPE))
+	{
+		//当たったときの距離の代入
+		//distance_=
+	}
+}
+
+IFE::Float3 PlayerActionCamera::GetPos()
+{
+	return transform_->position_;
+}
+
+IFE::Camera* PlayerActionCamera::GetCamera()
+{
+	return actionCamera_;
+}
+
+void PlayerActionCamera::CameraMove(const IFE::Vector3& playerPos)
+{
+	//補間時間調整値
+	const float adjustedTimeValue = 15.0f;
+	//カメラのY座標調節値
+	const float cameraYAdd = 7.5f;
+
+	//視点の移動先の座標
+	IFE::Vector3 eyeDestinationPos =
+	{
+		playerPos.x + distance_ * cosf(IFE::ConvertToRadians(cameraAngle_.x)) * sinf(IFE::ConvertToRadians(cameraAngle_.y)),
+		playerPos.y + cameraYAdd + distance_ * sinf(IFE::ConvertToRadians(cameraAngle_.x)),
+		playerPos.z + distance_ * cosf(IFE::ConvertToRadians(cameraAngle_.x)) * cosf(IFE::ConvertToRadians(cameraAngle_.y))
+	};
+
+	//視点の補間移動
+	IFE::Complement(transform_->position_.x, eyeDestinationPos.x, adjustedTimeValue);
+	IFE::Complement(transform_->position_.y, eyeDestinationPos.y, adjustedTimeValue);
+	IFE::Complement(transform_->position_.z, eyeDestinationPos.z, adjustedTimeValue);
+
+	actionCamera_->transform_->eye_ = transform_->position_;
+
+	//注視点の移動先の座標
+	IFE::Vector3 targetDestinationPos =
+	{
+		playerPos.x - distance_ * cosf(IFE::ConvertToRadians(cameraAngle_.x)) * sinf(IFE::ConvertToRadians(cameraAngle_.y)),
+		playerPos.y - distance_ * sinf(IFE::ConvertToRadians(cameraAngle_.x)),
+		playerPos.z - distance_ * cosf(IFE::ConvertToRadians(cameraAngle_.x)) * cosf(IFE::ConvertToRadians(cameraAngle_.y))
+	};
+
+	//注視点の補間移動
+	IFE::Complement(actionCamera_->transform_->target_.x, targetDestinationPos.x, adjustedTimeValue);
+	IFE::Complement(actionCamera_->transform_->target_.y, targetDestinationPos.y, adjustedTimeValue);
+	IFE::Complement(actionCamera_->transform_->target_.z, targetDestinationPos.z, adjustedTimeValue);
+}
+
+void PlayerActionCamera::CameraRot()
+{
+	//回転速度
+	const float rotSpeed = 80.0f;
+
+#pragma region キーボード
+	if (IFE::Input::GetKeyPush(IFE::Key::LEFT))
+	{
+		cameraAngle_.y -= rotSpeed * IFE::IFETime::sDeltaTime_;
+	}
+	if (IFE::Input::GetKeyPush(IFE::Key::RIGHT))
+	{
+		cameraAngle_.y += rotSpeed * IFE::IFETime::sDeltaTime_;
+	}
+	if (IFE::Input::GetKeyPush(IFE::Key::UP))
+	{
+		cameraAngle_.x -= rotSpeed * IFE::IFETime::sDeltaTime_;
+	}
+	if (IFE::Input::GetKeyPush(IFE::Key::DOWN))
+	{
+		cameraAngle_.x += rotSpeed * IFE::IFETime::sDeltaTime_;
+	}
+#pragma endregion キーボード
+
+#pragma region コントローラー
+
+	cameraAngle_ += {-IFE::Input::GetRYAnalog(controllerRange_) * rotSpeed * IFE::IFETime::sDeltaTime_, IFE::Input::GetRXAnalog(controllerRange_)* rotSpeed* IFE::IFETime::sDeltaTime_};
+
+#pragma endregion コントローラー
+
+	//縦回転限界処理
+	cameraAngle_.x = std::clamp(cameraAngle_.x, -60.0f, 60.0f);
+}
+
+void PlayerActionCamera::CameraUpdate(const IFE::Vector3& playerPos)
+{
+	//カメラの回転
+	CameraRot();
+
+	//カメラ補間
+	CameraMove(playerPos);
 }
--- PlayerActionCamera.h
+++ PlayerActionCamera.h
@@ -1,11 +1,20 @@
 #pragma once
 #include "Component.h"
+#include"CameraManager.h"
 
 class PlayerActionCamera:public IFE::Component
 {
 private:
 
+	IFE::Camera* actionCamera_ = nullptr;
 
+	IFE::Vector3 cameraPos_ = {};
+	IFE::Vector2 cameraAngle_ = {};
+
+	//ターゲットからの距離
+	float distance_ = 15.0f;
+
+	const uint16_t controllerRange_ = 10000;
 
 public:
 
@@ -13,9 +22,21 @@ public:
 
 	void Update();
 
+	void CameraUpdate(const IFE::Vector3& playerPos);
+
 	void Draw();
 
 	void Finalize();
 
-	void OnColliderHit(IFE::ColliderCore collider);
+	void OnColliderHit(IFE::ColliderCore* myCollider, IFE::ColliderCore* hitCollider)override;
+
+	IFE::Float3 GetPos();
+
+	IFE::Camera* GetCamera();
+
+private:
+
+	void CameraMove(const IFE::Vector3& playerPos);
+
+	void CameraRot();
 };
