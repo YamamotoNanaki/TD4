70b9c2f1b805e82a6dd632f282dc703e24306b06
LC1A_21_Yamamoto_Nanaki <k021g1320@g.neec.ac.jp>
2024-06-28 10:38:55 +0900

    コンポーネントの現在使用不可の仮想関数を通常の関数に変更

--- Engine/Component/Component.h
+++ Engine/Component/Component.h
@@ -58,7 +58,7 @@ namespace IFE
 		virtual void CopyValueComponent(Component* component);
 #ifdef InverseEditorMode
 #else
-		virtual void DebugGUI(uint32_t num);
+		void DebugGUI(uint32_t num);
 		virtual void ComponentDebugGUI() {};
 		void OutputScene(nlohmann::json& json);
 		virtual void OutputComponent(nlohmann::json& json);
