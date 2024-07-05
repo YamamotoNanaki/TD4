87b1ad919c7bf1c8f6160e3d34f5d056bb7eef9d
sano_yuya <k021g1256@g.neec.ac.jp>
2024-07-05 16:47:02 +0900

    一応コメントアウト追加

--- Source/Config/PoseMenu.cpp
+++ Source/Config/PoseMenu.cpp
@@ -17,8 +17,9 @@ void PoseMenu::Initialize()
 
 void PoseMenu::Update()
 {
+	//ポーズ切り替え
 	PoseChange();
-
+	//ポーズ中選択
 	PoseSelect();
 }
 
@@ -36,8 +37,10 @@ void PoseMenu::PoseChange()
 	if (IFE::Input::GetKeyTrigger(IFE::Key::Esc) || IFE::Input::PadTrigger(IFE::PADCODE::START))
 	{
 		poseFlag_ = !poseFlag_;
+		//画面を暗くするスプライトの描画
 		IFE::SpriteManager::Instance()->GetSpritePtr("BlackBack")->drawFlag_ = poseFlag_;
 
+		//時間を止める処理
 		if (poseFlag_ == true)
 		{
 			IFE::IFETime::sTimeScale_ = 0.0f;
@@ -54,7 +57,7 @@ void PoseMenu::PoseSelect()
 	int32_t range = 10000;
 	if (poseFlag_ == true)
 	{
-		if (IFE::Input::GetKeyTrigger(IFE::Key::UP) || oldLAnalog_ < 0.5f && IFE::Input::GetLYAnalog(range)>0.5f)
+		if (IFE::Input::GetKeyTrigger(IFE::Key::UP) || oldLAnalog_ < 0.5f && IFE::Input::GetLYAnalog(range) > 0.5f)
 		{
 			selectNum_--;
 		}
@@ -62,6 +65,7 @@ void PoseMenu::PoseSelect()
 		{
 			selectNum_++;
 		}
+		//フラグが限界を超えない処理
 		selectNum_ = std::clamp(selectNum_, minSelectNum_, maxSelectNum_);
 	}
 	else
@@ -70,6 +74,7 @@ void PoseMenu::PoseSelect()
 		selectNum_ = 0;
 	}
 
+	//選択中のintの値をselectFlag_に同期させる処理
 	selectFlag_ = static_cast<PoseSelectFlag>(selectNum_);
 	oldLAnalog_ = IFE::Input::GetLYAnalog(range);
 }
