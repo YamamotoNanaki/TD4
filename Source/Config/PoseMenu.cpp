433fabcda30c9bb6c441d4c2b8d147ea5adf3b19
sano_yuya <k021g1256@g.neec.ac.jp>
2024-07-05 16:30:08 +0900

    LスティックUPDOWN多分おそらくきっとこれでよし

--- Source/Config/PoseMenu.cpp
+++ Source/Config/PoseMenu.cpp
@@ -17,20 +17,9 @@ void PoseMenu::Initialize()
 
 void PoseMenu::Update()
 {
-	if (IFE::Input::GetKeyTrigger(IFE::Key::Esc) || IFE::Input::PadTrigger(IFE::PADCODE::START))
-	{
-		poseFlag_ = !poseFlag_;
-		IFE::SpriteManager::Instance()->GetSpritePtr("BlackBack")->drawFlag_ = poseFlag_;
-	}
+	PoseChange();
 
-	if (poseFlag_ == true)
-	{
-		IFE::IFETime::sTimeScale_ = 0.0f;
-	}
-	else
-	{
-		IFE::IFETime::sTimeScale_ = 1.0f;
-	}
+	PoseSelect();
 }
 
 void PoseMenu::Finalize()
@@ -40,4 +29,47 @@ void PoseMenu::Finalize()
 bool PoseMenu::GetPoseFlag()
 {
 	return poseFlag_;
+}
+
+void PoseMenu::PoseChange()
+{
+	if (IFE::Input::GetKeyTrigger(IFE::Key::Esc) || IFE::Input::PadTrigger(IFE::PADCODE::START))
+	{
+		poseFlag_ = !poseFlag_;
+		IFE::SpriteManager::Instance()->GetSpritePtr("BlackBack")->drawFlag_ = poseFlag_;
+
+		if (poseFlag_ == true)
+		{
+			IFE::IFETime::sTimeScale_ = 0.0f;
+		}
+		else
+		{
+			IFE::IFETime::sTimeScale_ = 1.0f;
+		}
+	}
+}
+
+void PoseMenu::PoseSelect()
+{
+	int32_t range = 10000;
+	if (poseFlag_ == true)
+	{
+		if (IFE::Input::GetKeyTrigger(IFE::Key::UP) || oldLAnalog_ < 0.5f && IFE::Input::GetLYAnalog(range)>0.5f)
+		{
+			selectNum_--;
+		}
+		else if (IFE::Input::GetKeyTrigger(IFE::Key::DOWN) || oldLAnalog_ > -0.5f && IFE::Input::GetLYAnalog(range) < -0.5f)
+		{
+			selectNum_++;
+		}
+		selectNum_ = std::clamp(selectNum_, minSelectNum_, maxSelectNum_);
+	}
+	else
+	{
+		selectFlag_ = PoseSelectFlag::RETURNTITLE;
+		selectNum_ = 0;
+	}
+
+	selectFlag_ = static_cast<PoseSelectFlag>(selectNum_);
+	oldLAnalog_ = IFE::Input::GetLYAnalog(range);
 }
