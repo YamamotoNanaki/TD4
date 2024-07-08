846ffecca426342bf9fbf5d5749e31f138babd4d
k021g1165@g.neec.ac.jp <k021g1165@g.neec.ac.jp>
2024-07-08 17:09:41 +0900

    map途中

--- Source/Map/MapUI.h
+++ Source/Map/MapUI.h
@@ -2,6 +2,7 @@
 #include "Component.h"
 #include "IFEMath.h"
 #include "Object3D.h"
+#include "Sprite.h"
 
 class MapUI:public IFE::Component
 {
@@ -29,6 +30,11 @@ public:
 private:
 
 	IFE::Object3D* playerObj_ = nullptr;
+
+	std::vector<IFE::Float2> objectPos_;
+	std::vector<float> objectRot_;
+
+	std::vector<IFE::Sprite*> mapSprite_;
 
 };
 
