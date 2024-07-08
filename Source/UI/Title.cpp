b790f8065840ad4ecd83fab39c0828693567e82e
DESKTOP-UBPBP6P\user <k021g1476@g.neec.ac.jp>
2024-07-08 17:11:21 +0900

    titlese

--- Source/UI/Title.cpp
+++ Source/UI/Title.cpp
@@ -1,13 +1,20 @@
 #include "Title.h"
 #include "input.h"
 #include "Scene.h"
+#include "Sound.h"
 
 using namespace IFE;
+
+//void Title::Initialize()
+//{
+//	//IFE::Sound::Instance()->LoadWave("title");
+//}
 
 void Title::Update()
 {
 	if (Input::PadTrigger(PADCODE::ALL) || Input::GetKeyTrigger(Key::Space))
 	{
+	/*	IFE::Sound::Instance()->SoundPlay(0, false, true);*/
 		Scene::Instance()->SetNextScene("stage01");
 	}
 }
