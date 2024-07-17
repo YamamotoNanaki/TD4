ebb1fc431d989e105713f27a77008395a99b39a8
LC1A_21_Yamamoto_Nanaki <k021g1320@g.neec.ac.jp>
2024-07-17 15:12:03 +0900

    プッシュで来てない

--- Source/Player/PlayerAction.cpp
+++ Source/Player/PlayerAction.cpp
@@ -274,7 +274,6 @@ void PlayerAction::Attack()
 				if (playerAttack_->GetIsBackAttack() == false)
 				{
 					ani_->SetAnimation("backKnifeAttack");//’ÊíUŒ‚ƒ‚[ƒVƒ‡ƒ“‚É•Ï‚¦‚é
-					slowFlag_ = true;
 				}
 				else
 				{
