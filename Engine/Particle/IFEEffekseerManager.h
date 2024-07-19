d9e7f832897a20320813205ca6a0d57055f07d1d
LC1A_21_Yamamoto_Nanaki <k021g1320@g.neec.ac.jp>
2024-07-19 16:55:06 +0900

    Effekseer途中

---
+++ Engine/Particle/IFEEffekseerManager.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#pragma warning(push, 0) 
+#include <Effekseer.h>
+#include <EffekseerRendererDX12.h>
+#pragma warning(pop) 
+
+namespace IFE
+{
+	class IFEEffekseerManager
+	{
+		void Initialize();
+	};
+}
