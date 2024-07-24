592e53fe0ec1b4a7144a665210606d9b9cd06251
sano_yuya <k021g1256@g.neec.ac.jp>
2024-07-24 15:44:16 +0900

    XボタンUIのセッター作った

--- Source/Player/PlayerAction.cpp
+++ Source/Player/PlayerAction.cpp
@@ -429,19 +429,19 @@ void PlayerAction::AttackUI()
 	{
 		if (closestEnemy != nullptr && IFE::Collision::CheckCircle({ {transform_->position_.x,transform_->position_.z},5.0f }, { closestEnemy->GetPos(),1.0f }))
 		{
-			isAttackUI_ = true;
+			isXBottonUI_ = true;
 		}
 		else
 		{
-			isAttackUI_ = false;
+			isXBottonUI_ = false;
 		}
 	}
 	else
 	{
-		isAttackUI_ = false;
+		isXBottonUI_ = false;
 	}
 
-	IFE::SpriteManager::Instance()->GetSpritePtr("attackUI")->drawFlag_ = isAttackUI_;
+	IFE::SpriteManager::Instance()->GetSpritePtr("attackUI")->drawFlag_ = isXBottonUI_;
 }
 
 void PlayerAction::ApproachTarget(float& current, float target, float step)
@@ -459,7 +459,7 @@ void PlayerAction::ApproachTarget(float& current, float target, float step)
 
 void PlayerAction::AutoAim()
 {
-	if (isAttackUI_ == true)
+	if (isXBottonUI_ == true)
 	{
 		IFE::Vector3 frontVec = closestEnemy->transform_->position_ - transform_->transform_->position_;
 		playerAttack_->objectPtr_->transform_->position_ =
@@ -569,6 +569,11 @@ void PlayerAction::CrouchAnimation()
 
 		crouchAnimationTimer_ += IFE::IFETime::sDeltaTime_;
 	}
+}
+
+void PlayerAction::SetIsXBottonUI(bool flag)
+{
+	isXBottonUI_ = flag;
 }
 
 #ifdef EditorMode
