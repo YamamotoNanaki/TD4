e583572e44ce2215701c90826f5ac1b96a1ad405
sano_yuya <k021g1256@g.neec.ac.jp>
2024-07-31 12:35:16 +0900

    タイトル(ステージ選択はまだ)とゲームオーバーのときのUIの処理書いた

--- Source/UI/Title.h
+++ Source/UI/Title.h
@@ -14,20 +14,28 @@ private:
 
 	TitleSelect titleSelectFlag_ = TitleSelect::TITLE;
 
+	//タイトルロゴ
+	float titleAnimationTimer_ = 0.0f;
+	float beforeGameLogoPosY_ = 0.0f;
+
+	//カメラ
 	IFE::Camera* camera_ = nullptr;
 	float cameraAngle_ = 0.0f;
 	float beforeEaseAngle_ = 0.0f;
 	float distanse_ = 120.0f;
 
+	//タイトルからセレクトへの遷移
 	bool animationFlag_ = false;
 	float animationTime_ = 0.0f;
 	const float maxAnimationTime_ = 2.0f;
 
+	//ステージ選択
 	int8_t stageNum_ = 0;
 	int8_t minStageNum_ = 1;
 	int8_t maxStageNum_ = 3;
 	float oldLAnalog_ = 0.0f;
 
+	//最終確認
 	bool checkFlag_ = false;
 
 public:
@@ -39,6 +47,7 @@ private:
 
 private:
 
+	void TitleLogoSimpleHarmonicMotion();
 	void CameraRot();
 
 	void ToSelectAnimation();
